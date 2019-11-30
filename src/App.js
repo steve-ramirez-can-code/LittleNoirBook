6a35afb4015184ae62cd3e59311fd9f172fd8c7c
Steve <steveramirez@ac.c4q.nyc>
2019-11-29 20:31:18 -0500

    Testing deployment

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Hi Steve. You're the best!
         </a>
       </header>
     </div>
