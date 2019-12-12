e87ce381fb4f6c1dc85a65f6b9f12578f78442b3
Steve <steveramirez@ac.c4q.nyc>
2019-12-12 18:14:28 -0500

    addition of sequelize and postgres

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', function(req, res, next) {
+  res.send('Testing')
+//   console.log(req.body);
+});
+
+
+
 module.exports = router;
